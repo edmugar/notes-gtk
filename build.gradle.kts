669def61395cdbf03ceb0d3cddb421ffe73a8d64
edmugar <edgarciamuoz@gmail.com>
2025-09-13 22:14:07 -0600

    Add: GTK dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.github.bailuk:java-gtk:0.5.0")
 }
 
 tasks.test {
