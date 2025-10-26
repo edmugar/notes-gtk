e4f6bdc0ec1b9ac1b24902e252f73738ddcf3172
edmugar <edgarciamuoz@gmail.com>
2025-10-26 11:36:56 -0600

    Add: Bucle for when create a new note

--- src/main/java/org/robote/Main.java
+++ src/main/java/org/robote/Main.java
@@ -11,16 +11,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List list=new List();
-    Scanner in=new Scanner(System.in);
-        Note newNote=new Note();
-        System.out.println("Ingres el Titulo de la nota:");
-        newNote.setTitle(in.nextLine());
-        System.out.println("Ingrese el contenido de la nota:");
-        newNote.setContent(in.nextLine());
-        System.out.println("----------------------------------");
-
-        list.NewNote(newNote);
+        List list = new List();
+        Scanner in = new Scanner(System.in);
+        Note newNote = new Note();
+        System.out.println("Ingrese el número de notas a agregar: ");
+        int loop_number = in.nextInt();
+        
+        for (int i = 0; i < loop_number; i++) {
+            System.out.println("Ingrese el Titulo de la nota:");
+            newNote.setTitle(in.nextLine());
+            System.out.println("Ingrese el contenido de la nota:");
+            newNote.setContent(in.nextLine());
+            System.out.println("----------------------------------");
+            list.NewNote(newNote);
+        }
 
         list.ShowNote();
     }
